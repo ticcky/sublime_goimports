d2f2b7b5659cda97904d228bfdbc4d26edb4e0bf
Lukas Zilka <lukas@zilka.me>
2013-11-19 08:35:24 -0500

    added settings

--- GoImports.py
+++ GoImports.py
@@ -1,21 +1,27 @@
 # Sublime3 Plugin that runs goimports on the current file
 #
 # Author: Lukas Zilka (lukas@zilka.me)
-# 
+#
 import sublime
 import sublime_plugin
 import subprocess
 import io
 
 
+s = sublime.load_settings("GoImports.sublime-settings")
+
+
 class GoImportsCommand(sublime_plugin.TextCommand):
     def run(self, edit, saving=False):
+        # Get the content of the current window from the text editor.
         selection = sublime.Region(0, self.view.size())
         content = self.view.substr(selection)
-        process = subprocess.Popen(['goimports'], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
 
+        # Shove that content down goimports process's throat.
+        process = subprocess.Popen([s.get("go_imports_bin_path")], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
         process.stdin.write(bytes(content, 'utf8'))
         process.stdin.close()
         process.wait()
 
+        # Put the result back.
         self.view.replace(edit, selection, process.stdout.read().decode('utf8'))
